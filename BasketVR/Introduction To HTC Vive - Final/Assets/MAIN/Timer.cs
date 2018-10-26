d3445402186501c45d0a0acc4c7007c59c0a0630
DanielMoraC <43269179+DanielMoraC@users.noreply.github.com>
2018-10-26 12:07:42 +0200

    h

---
+++ BasketVR/Introduction To HTC Vive - Final/Assets/MAIN/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    float timeLeft = 90;
+
+    public Text tiempo;
+
+    private bool contando = false;
+
+    void Update()
+    {        
+        tiempo.text = "Seconds remaining: " + Mathf.Round(timeLeft);
+
+        if (contando)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+
+        if (timeLeft <= 0)
+        {
+            print("Fin");
+        }
+    }
+
+    public void PlayGame()
+    {
+           
+    }
+}
